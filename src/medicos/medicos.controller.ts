f5a5ff6d611ef031bae06d0276b622528a5fce79
eSeylaA69 <andyvt0304@gmail.com>
2026-01-20 14:54:55 -0500

    test2

--- src/medicos/medicos.controller.ts
+++ src/medicos/medicos.controller.ts
@@ -26,7 +26,7 @@ export class MedicosController {
     }
 
     @Roles(Rol.Medico)
-    @Get('myInfoTest')
+    @Get('myInfoTest2')
     @HttpCode(HttpStatus.OK)
     async getMyInfo(@Request() req: UserRequest): Promise<PerfilMedico> {
         return await this.medicosService.myInfo(req.user.id);
